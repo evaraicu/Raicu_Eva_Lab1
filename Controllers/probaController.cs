ffb4309dc7bb8bd5da670986c93ff1282eda6801
evaraicu <evamaria.2003@yahoo.com>
2025-12-17 23:27:44 +0200

    probaController

---
+++ Controllers/probaController.cs
@@ -0,0 +1,18 @@
+﻿using System.Drawing;
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Raicu_Eva_Lab1.Controllers
+{
+    public class probaController : Controller
+    {
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+    }
+}
